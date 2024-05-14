3d0ee5cb61ed8a3b24e4e3aee1ab1ca516dbd0c9
ojea <ojeaxe@gmail.com>
2024-05-14 09:23:26 +0200

    Add project files.

---
+++ MvcCoreAWSPostgresEC2/Controllers/DepartamentosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSPostgresEC2.Models;
+using MvcCoreAWSPostgresEC2.Repositories;
+
+namespace MvcCoreAWSPostgresEC2.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryDepartamentos repo;
+
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Departamento> departamentos = await this.repo.GetDepartamentoAsync();
+            return View(departamentos);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Departamento departamentos = await this.repo.FindDepartamentoAsync(id);
+            return View(departamentos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Departamento dept)
+        {
+            await this.repo.InsertarDepartamento(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return RedirectToAction("Index");
+        }
+    }
+}
